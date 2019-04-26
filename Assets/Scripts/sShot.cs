ad61cb14e2d4da15cef13e090ed17df36aeac697
kar.saadi@gmail.com <Sunsetgit3!>
2019-04-26 18:25:49 +0200

    shooting
    shooting and better player position

---
+++ Assets/Scripts/sShot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sShot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object.Destroy(gameObject, 0.8f);
+        Object.Destroy(transform.parent.gameObject, 0.8f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
